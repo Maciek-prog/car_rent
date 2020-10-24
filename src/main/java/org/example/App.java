944685b24ed157205585c29808baf5f280ddeb41
Maciek-prog <pretkimaciej1@gmail.com>
2020-10-24 12:23:53 +0200

    App update , CarDao, add MainMenu

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,18 @@
 package org.example;
 
-public class App {
+import pl.sda.dao.CustomersDao;
+import pl.sda.dao.RentingDao;
+import pl.sda.model.Car;
+import pl.sda.model.Customers;
 
+public class App {
+    public static void main(String[] args) {
+        CustomersDao customersDao = new CustomersDao();
+        RentingDao rentingDao = new RentingDao();
+        Customers customers = new Customers(1,"Mariusz", "Kowalski");
+        Customers customers1 = new Customers(2,"Lucek", "Mieczewski");
+        customersDao.add(customers);
+        customersDao.add(customers1);
+
+    }
 }
---
+++ src/main/java/pl/sda/dao/CarDao.java
@@ -0,0 +1,10 @@
+package pl.sda.dao;
+
+import pl.sda.model.Car;
+
+public class CarDao extends AbstractDao<Car> {
+
+    public CarDao(Class<Car> clazz) {
+        super(clazz);
+    }
+}
