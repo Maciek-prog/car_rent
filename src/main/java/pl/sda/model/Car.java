5587986c878dbf8312ed2a4ac9c029ea00299ab0
Maciek-prog <pretkimaciej1@gmail.com>
2020-10-24 10:47:18 +0200

    update

--- src/main/java/pl/sda/model/Car.java
+++ src/main/java/pl/sda/model/Car.java
@@ -1,4 +1,70 @@
 package pl.sda.model;
 
 public class Car {
+    private int id;
+    private String marka;
+    private String model;
+    private String color;
+    private String nr_rejestracyjny;
+
+    public Car(int id, String marka, String model, String color, String nr_rejestracyjny) {
+        this.id = id;
+        this.marka = marka;
+        this.model = model;
+        this.color = color;
+        this.nr_rejestracyjny = nr_rejestracyjny;
+    }
+
+    public String getMarka() {
+        return marka;
+    }
+
+    public void setMarka(String marka) {
+        this.marka = marka;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getNr_rejestracyjny() {
+        return nr_rejestracyjny;
+    }
+
+    public void setNr_rejestracyjny(String nr_rejestracyjny) {
+        this.nr_rejestracyjny = nr_rejestracyjny;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", marka='" + marka + '\'' +
+                ", model='" + model + '\'' +
+                ", color='" + color + '\'' +
+                ", nr_rejestracyjny='" + nr_rejestracyjny + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
--- src/main/java/pl/sda/model/Customers.java
+++ src/main/java/pl/sda/model/Customers.java
@@ -1,4 +1,56 @@
 package pl.sda.model;
 
 public class Customers {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Customers(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customers{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
