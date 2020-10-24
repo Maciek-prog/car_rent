7278c2847076af95a3d9bd0f4dcaba771077fba5
Maciek-prog <pretkimaciej1@gmail.com>
2020-10-24 11:12:38 +0200

    refactor to SessionProvider

---
+++ src/main/java/pl/sda/database/SessionProvider.java
@@ -0,0 +1,13 @@
+package pl.sda.database;
+
+import org.hibernate.cfg.Configuration;
+
+public class SessionProvider {
+//        private static final SessionProvider sessionProvider = new Configuration().configure().buildSessionFactory();
+//        private SessionProvider(){
+//        }
+//
+//    public static SessionProvider getSessionProvider() {
+//        return sessionProvider;
+//    }
+}
