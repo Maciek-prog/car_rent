909861f5110c7982d7cf596ff529f23649410c29
Maciek-prog <pretkimaciej1@gmail.com>
2020-10-25 11:20:14 +0100

    SessionProvider

--- src/main/java/pl/sda/database/SessionProvider.java
+++ src/main/java/pl/sda/database/SessionProvider.java
@@ -2,9 +2,24 @@ package pl.sda.database;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.BootstrapServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
+import javax.imageio.spi.ServiceRegistry;
+
 public class SessionProvider {
+//    private static SessionFactory sessionFactory;
+//    private static ServiceRegistry serviceRegistry;
+//
+//    public static SessionFactory createSessionFactory() {
+//        Configuration configuration = new Configuration();
+//        configuration.configure();
+//        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().appSetting(
+//                configuration.getProperties()). buildServiceRegistry();
+//        sessionFactory = configuration.buildSessionFactory((org.hibernate.service.ServiceRegistry) serviceRegistry);
+//        return sessionFactory;
+//    }
+//}
     private static final SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 
     private SessionProvider() {
