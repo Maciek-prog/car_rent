9f91aa4b34da07844ab6c100c7db6ff56056ac11
Maciek-prog <pretkimaciej1@gmail.com>
2020-10-25 13:04:00 +0100

    Update MainMenu

--- src/main/java/pl/sda/MainMenu/MainMenu.java
+++ src/main/java/pl/sda/MainMenu/MainMenu.java
@@ -1,5 +1,21 @@
 package pl.sda.MainMenu;
 
 public class MainMenu {
+    public static void bazaDanych() {
+
+    }
+
+
+
+    public static void Menu() {
+        System.out.println();
+        System.out.println("     ****************************************");
+        System.out.println("     *                 MENU                 *");
+        System.out.println("     ****************************************");
+        System.out.println("    1. Baza Danych");
+        System.out.println("    2. Informacje ");
+        System.out.println("    0.Koniec");
+
+    }
 
 }
